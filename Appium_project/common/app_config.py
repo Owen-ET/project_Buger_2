c9b9f8c15fbc3cba866620c38d8de17848eb1783
zhangchang <zhangchanget@126.com>
2019-12-28 18:25:31 +0800

    12.28号小程序自动化代码上传

--- Appium_project/common/app_config.py
+++ Appium_project/common/app_config.py
@@ -5,20 +5,35 @@
 # @File    : app_config.py
 
 from appium import webdriver
+import yaml
+import os
 
 def des_caps():
 
-    Cap = {
-        "deviceName":"vivo",
-        "platformName":"Android",
-        "platformVersion":"8.1",
-        "appPackage":"com.csksc2b.invertory",
-        "appActivity":"com.csks.login.SplashAty",
-        # "noReset":True
-        "automationName":"UiAutomator2"
-    }
+    # Cap = {
+    #     "deviceName":"vivo",
+    #     "platformName":"Android",
+    #     "platformVersion":"8.1",
+    #     "appPackage":"com.csksc2b.invertory",
+    #     "appActivity":"com.csks.login.SplashAty",
+    #     # "noReset":True
+    #     "automationName":"UiAutomator2"
+    # }
 
-    driver = webdriver.Remote("http://localhost:4723/wd/hub",Cap)
+    # 基础路径
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    # yaml路径
+    yaml_path = base_dir + "/data/ddt_data_file.yaml"
+    # 获取yaml的数据
+    with open(yaml_path,'r',encoding='utf-8') as file:
+        data = yaml.load(file)
+    start = data['start_HRApp']
+    Cap = start['caps']['android']
+
+    driver = webdriver.Remote("http://"+ str(start['ip']) +":"+ str(start['port']) +"/wd/hub",Cap)
     driver.implicitly_wait(10)
 
     return driver
+
+
+des_caps()
